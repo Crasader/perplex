c04324191c6cbfe47588eb784b893aee596ce081
wmzh2006 <wmzh0003@qq.com>
2015-04-21 16:26:29 +0800

    create

--- MapEditor/play - 副本.bat
+++
@@ -1,4 +0,0 @@
-cls
-@echo on
-java -classpath class -Xmx512m %TARGET%
-@echo off
---
+++ PacketMap.bat
@@ -0,0 +1,10 @@
+
+
+copy .\MapEditor\save\building\l0_building_mobile.dat .\Resources\mapdat\l0_building_mobile.dat
+copy .\MapEditor\save\floor\l0_floor_mobile.dat .\Resources\mapdat\l0_floor_mobile.dat
+copy .\MapEditor\save\unit\l0_unit_mobile.dat .\Resources\mapdat\10_unit_mobile.dat
+copy .\MapEditor\save\rect\l0_rect_mobile.dat .\Resources\mapdat\10_rect_mobile.dat
+copy .\MapEditor\save\event\10_event_mobile.dat .\Resources\mapdat\10_event_mobile.dat
+copy .\MapEditor\save\mapinfo\l0_mapinfo_mobile.dat .\Resources\mapdat\10_mapinfo_mobile.dat
+
+pause
