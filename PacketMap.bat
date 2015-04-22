cf9cc9a7b07a0bac7e2eaec196d159a2ccb6497b
wmzh2006 <wmzh0003@qq.com>
2015-04-22 17:39:25 +0800

    create

--- PacketMap.bat
+++ PacketMap.bat
@@ -7,4 +7,11 @@ copy .\MapEditor\save\rect\l0_rect_mobile.dat .\Resources\mapdat\l0_rect_mobile.
 copy .\MapEditor\save\event\l0_event_mobile.dat .\Resources\mapdat\l0_event_mobile.dat
 copy .\MapEditor\save\mapinfo\l0_mapinfo_mobile.dat .\Resources\mapdat\l0_mapinfo_mobile.dat
 
+copy .\MapEditor\save\building\l1_building_mobile.dat .\Resources\mapdat\l1_building_mobile.dat
+copy .\MapEditor\save\floor\l1_floor_mobile.dat .\Resources\mapdat\l1_floor_mobile.dat
+copy .\MapEditor\save\unit\l1_unit_mobile.dat .\Resources\mapdat\l1_unit_mobile.dat
+copy .\MapEditor\save\rect\l1_rect_mobile.dat .\Resources\mapdat\l1_rect_mobile.dat
+copy .\MapEditor\save\event\l1_event_mobile.dat .\Resources\mapdat\l1_event_mobile.dat
+copy .\MapEditor\save\mapinfo\l1_mapinfo_mobile.dat .\Resources\mapdat\l1_mapinfo_mobile.dat
+
 pause
